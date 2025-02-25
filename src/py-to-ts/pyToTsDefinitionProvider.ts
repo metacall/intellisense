5667ef908f0b13e64e1dd9dbcb715c4e3fcc4086
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-02-25 19:23:50 +0530

    feat: implement Python function extraction and update definition provider for improved functionality

--- src/py-to-ts/pyToTsDefinitionProvider.ts
+++ src/py-to-ts/pyToTsDefinitionProvider.ts
@@ -1,10 +1,11 @@
 import * as vscode from 'vscode';
-import * as cp from 'child_process';
 import * as path from 'path';
+import { spawn } from 'child_process';
+import * as fs from 'fs';
 
 export function registerPyDefinitionProvider(context: vscode.ExtensionContext) {
     const provider = vscode.languages.registerDefinitionProvider(
-        [{ language: 'typescript' }, { language: 'javascript' }], // Now works for both JS and TS
+        [{ language: 'typescript' }, { language: 'javascript' }],
         {
             provideDefinition(document, position, token) {
                 const range = document.getWordRangeAtPosition(position);
@@ -12,6 +13,7 @@ export function registerPyDefinitionProvider(context: vscode.ExtensionContext) {
 
                 const word = document.getText(range);
                 const filePath = document.uri.fsPath;
+                console.log({ filePath });
 
                 // Check if the function exists in Python, regardless of the file type
                 const pythonLocation = findPythonDefinition(word);
@@ -35,6 +37,36 @@ export function registerPyDefinitionProvider(context: vscode.ExtensionContext) {
 
 
 function findPythonDefinition(symbol: string): { file: string, line: number } | null {
+
+    // TODO: instead of making a new json for function goto definitions, put all this data in the language ast json itself and use that
+
+    const workspaceFolder = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : '';
+    const pyFiles = fs.readdirSync(workspaceFolder).filter(file => file.endsWith('.py'));
+
+    let scriptPath = path.join(__dirname, '..', '..', 'src', 'py-to-ts', 'pyToTsGotoDefinitions.py');
+
+    if (workspaceFolder) {
+        pyFiles.forEach(pyFile => {
+            const sourcePyFilePath = path.join(workspaceFolder, pyFile);
+
+            console.log({ sourcePyFilePath });
+
+            const pythonProcess = spawn("python", [sourcePyFilePath]);
+
+            pythonProcess.stdout.on("data", (data) => {
+                console.log(`Output: ${data.toString()}`);
+            });
+
+            pythonProcess.stderr.on("data", (data) => {
+                console.error(`Error: ${data.toString()}`);
+            });
+
+            pythonProcess.on("close", (code) => {
+                console.log(`Process exited with code ${code}`);
+            });
+        });
+    }
+
     const functionMap: Record<string, { file: string, line: number }> = {
         "add": {
             // "file": "/Users/abhinavmishra/Coding/mc-int/sandbox/goto-def/math_utils.py",
